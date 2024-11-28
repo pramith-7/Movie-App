2cf79a6467a0aa3cce37b2bc0e2e3dc434f65181
E.A.P.P.EDIRISOORIYA <121412462+pramith-7@users.noreply.github.com>
2024-11-28 18:27:30 +0530

    Initial commit: Added frontend and backend folders

---
+++ movie-gold-v1/src/components/home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Hero from '../hero/hero';
+
+const Home = () => {
+    return (
+        <Hero movies = {movies} />
+    )
+}
+
+export default Home
---
+++ movies/src/main/java/dev/pr7/movies/MoviesApplication.java
@@ -0,0 +1,11 @@
+package dev.pr7.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MoviesApplication {
+
+	public static void main(String[] args) {SpringApplication.run(MoviesApplication.class, args);}
+}
